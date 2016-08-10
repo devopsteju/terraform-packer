b5a171e86691847f096c025fb170c42c3c6006a1
Gianni Dalla Torre <gianni@ebi.ac.uk>
2016-08-10 16:01:17 +0100

    Add variables, defaults and example file for AWS

---
+++ centos7-aws/variables.tf
@@ -0,0 +1,39 @@
+variable "name" {
+  default     = "centos7-aws-jenkins"
+  description = "The name of the project"
+}
+
+variable "region" {
+  type    = "string"
+  default = "eu-west-1"
+}
+
+variable "access_key" {}
+
+variable "secret_key" {}
+
+variable "ami" {
+  default = "ami-7abd0209"
+}
+
+variable "ssh_public_key" {
+  description = "The ssh public key to inject in the host"
+}
+
+variable "ssh_public_key_name" {
+  type        = "string"
+  description = "The name of the ssh public key of the user"
+}
+
+variable "machine_type" {
+  default = "c4.small"
+}
+
+variable "volume_type" {
+  default = "st1"
+}
+
+variable "volume_size" {
+  description = "Disk size in GB"
+  default     = "10"
+}
