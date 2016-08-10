af5a8dded0f5e61c5770c2dc4bc43558c56d39c7
Gianni Dalla Torre <gianni@ebi.ac.uk>
2016-08-10 13:34:32 +0100

    Add provider file for AWS

---
+++ centos7-aws/provider.tf
@@ -0,0 +1,11 @@
+# Configure the AWS Provider
+provider "aws" {
+  region     = "${var.region}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+}
+
+resource "aws_key_pair" "aws" {
+  key_name   = "${var.ssh_public_key_name}"
+  public_key = "${var.ssh_public_key}"
+}
